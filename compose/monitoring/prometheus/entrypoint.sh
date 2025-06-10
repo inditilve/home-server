92ea35b22881c2dde9cb4c0316175161c2ad95d0
inditilve <indi.tilve@gmail.com>
2025-06-10 14:35:47 +0800

    install gettext-base to enable envsubst inside prom container

--- compose/monitoring/prometheus/entrypoint.sh
+++ compose/monitoring/prometheus/entrypoint.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+apt-get update && apt-get install -y gettext-base
 envsubst < /etc/prometheus/prometheus.yml.template > /etc/prometheus/prometheus.yml
 exec prometheus --config.file=/etc/prometheus/prometheus.yml
